2b7cc4dc43e010d02e1ca79a585415d54d0e565f
Cameron Stocks <87984443+CameronCS@users.noreply.github.com>
2022-11-23 01:50:40 +0200

    Add files via upload

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -23,12 +23,228 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ImgHangMan = new System.Windows.Forms.PictureBox();
+            this.LBLTrashHeading = new System.Windows.Forms.Label();
+            this.TBCMain = new System.Windows.Forms.TabControl();
+            this.TPMenu = new System.Windows.Forms.TabPage();
+            this.BTNPlay = new System.Windows.Forms.Button();
+            this.TPDifficulty = new System.Windows.Forms.TabPage();
+            this.BTNHard = new System.Windows.Forms.Button();
+            this.BTNMedium = new System.Windows.Forms.Button();
+            this.BTNEasy = new System.Windows.Forms.Button();
+            this.LBLDifficulty = new System.Windows.Forms.Label();
+            this.TPGame = new System.Windows.Forms.TabPage();
+            this.BtnPlayAgain = new System.Windows.Forms.Button();
+            this.BtnBack = new System.Windows.Forms.Button();
+            this.LBLLetters = new System.Windows.Forms.Label();
+            this.LBLUsed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgHangMan)).BeginInit();
+            this.TBCMain.SuspendLayout();
+            this.TPMenu.SuspendLayout();
+            this.TPDifficulty.SuspendLayout();
+            this.TPGame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ImgHangMan
+            // 
+            this.ImgHangMan.Location = new System.Drawing.Point(242, 25);
+            this.ImgHangMan.Name = "ImgHangMan";
+            this.ImgHangMan.Size = new System.Drawing.Size(234, 237);
+            this.ImgHangMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImgHangMan.TabIndex = 0;
+            this.ImgHangMan.TabStop = false;
+            this.ImgHangMan.Click += new System.EventHandler(this.ImgHangMan_Click);
+            // 
+            // LBLTrashHeading
+            // 
+            this.LBLTrashHeading.AutoSize = true;
+            this.LBLTrashHeading.Location = new System.Drawing.Point(53, 40);
+            this.LBLTrashHeading.Name = "LBLTrashHeading";
+            this.LBLTrashHeading.Size = new System.Drawing.Size(115, 20);
+            this.LBLTrashHeading.TabIndex = 1;
+            this.LBLTrashHeading.Text = "Incorrect Letters";
+            // 
+            // TBCMain
+            // 
+            this.TBCMain.Controls.Add(this.TPMenu);
+            this.TBCMain.Controls.Add(this.TPDifficulty);
+            this.TBCMain.Controls.Add(this.TPGame);
+            this.TBCMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TBCMain.Location = new System.Drawing.Point(0, 0);
+            this.TBCMain.Name = "TBCMain";
+            this.TBCMain.SelectedIndex = 0;
+            this.TBCMain.Size = new System.Drawing.Size(800, 450);
+            this.TBCMain.TabIndex = 2;
+            this.TBCMain.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TBCMain_KeyPress);
+            // 
+            // TPMenu
+            // 
+            this.TPMenu.Controls.Add(this.BTNPlay);
+            this.TPMenu.Location = new System.Drawing.Point(4, 29);
+            this.TPMenu.Name = "TPMenu";
+            this.TPMenu.Padding = new System.Windows.Forms.Padding(3);
+            this.TPMenu.Size = new System.Drawing.Size(792, 417);
+            this.TPMenu.TabIndex = 0;
+            this.TPMenu.Text = "TPMenu";
+            this.TPMenu.UseVisualStyleBackColor = true;
+            // 
+            // BTNPlay
+            // 
+            this.BTNPlay.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BTNPlay.Location = new System.Drawing.Point(284, 150);
+            this.BTNPlay.Name = "BTNPlay";
+            this.BTNPlay.Size = new System.Drawing.Size(216, 131);
+            this.BTNPlay.TabIndex = 0;
+            this.BTNPlay.Text = "Play";
+            this.BTNPlay.UseVisualStyleBackColor = true;
+            this.BTNPlay.Click += new System.EventHandler(this.BTNPlay_Click);
+            // 
+            // TPDifficulty
+            // 
+            this.TPDifficulty.Controls.Add(this.BTNHard);
+            this.TPDifficulty.Controls.Add(this.BTNMedium);
+            this.TPDifficulty.Controls.Add(this.BTNEasy);
+            this.TPDifficulty.Controls.Add(this.LBLDifficulty);
+            this.TPDifficulty.Location = new System.Drawing.Point(4, 29);
+            this.TPDifficulty.Name = "TPDifficulty";
+            this.TPDifficulty.Padding = new System.Windows.Forms.Padding(3);
+            this.TPDifficulty.Size = new System.Drawing.Size(792, 417);
+            this.TPDifficulty.TabIndex = 2;
+            this.TPDifficulty.Text = "Difficulty";
+            this.TPDifficulty.UseVisualStyleBackColor = true;
+            // 
+            // BTNHard
+            // 
+            this.BTNHard.Location = new System.Drawing.Point(606, 225);
+            this.BTNHard.Name = "BTNHard";
+            this.BTNHard.Size = new System.Drawing.Size(94, 29);
+            this.BTNHard.TabIndex = 3;
+            this.BTNHard.Text = "Hard";
+            this.BTNHard.UseVisualStyleBackColor = true;
+            this.BTNHard.Click += new System.EventHandler(this.BTNHard_Click);
+            // 
+            // BTNMedium
+            // 
+            this.BTNMedium.Location = new System.Drawing.Point(329, 225);
+            this.BTNMedium.Name = "BTNMedium";
+            this.BTNMedium.Size = new System.Drawing.Size(94, 29);
+            this.BTNMedium.TabIndex = 2;
+            this.BTNMedium.Text = "Medium";
+            this.BTNMedium.UseVisualStyleBackColor = true;
+            this.BTNMedium.Click += new System.EventHandler(this.BTNMedium_Click);
+            // 
+            // BTNEasy
+            // 
+            this.BTNEasy.Location = new System.Drawing.Point(8, 225);
+            this.BTNEasy.Name = "BTNEasy";
+            this.BTNEasy.Size = new System.Drawing.Size(94, 29);
+            this.BTNEasy.TabIndex = 1;
+            this.BTNEasy.Text = "Easy";
+            this.BTNEasy.UseVisualStyleBackColor = true;
+            this.BTNEasy.Click += new System.EventHandler(this.BTNEasy_Click);
+            // 
+            // LBLDifficulty
+            // 
+            this.LBLDifficulty.AutoSize = true;
+            this.LBLDifficulty.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LBLDifficulty.Location = new System.Drawing.Point(58, 33);
+            this.LBLDifficulty.Name = "LBLDifficulty";
+            this.LBLDifficulty.Size = new System.Drawing.Size(617, 112);
+            this.LBLDifficulty.TabIndex = 0;
+            this.LBLDifficulty.Text = "Select Difficulty";
+            // 
+            // TPGame
+            // 
+            this.TPGame.Controls.Add(this.BtnPlayAgain);
+            this.TPGame.Controls.Add(this.BtnBack);
+            this.TPGame.Controls.Add(this.LBLLetters);
+            this.TPGame.Controls.Add(this.LBLUsed);
+            this.TPGame.Controls.Add(this.ImgHangMan);
+            this.TPGame.Controls.Add(this.LBLTrashHeading);
+            this.TPGame.Location = new System.Drawing.Point(4, 29);
+            this.TPGame.Name = "TPGame";
+            this.TPGame.Padding = new System.Windows.Forms.Padding(3);
+            this.TPGame.Size = new System.Drawing.Size(792, 417);
+            this.TPGame.TabIndex = 1;
+            this.TPGame.Text = "Game";
+            this.TPGame.UseVisualStyleBackColor = true;
+            // 
+            // BtnPlayAgain
+            // 
+            this.BtnPlayAgain.Location = new System.Drawing.Point(690, 384);
+            this.BtnPlayAgain.Name = "BtnPlayAgain";
+            this.BtnPlayAgain.Size = new System.Drawing.Size(94, 29);
+            this.BtnPlayAgain.TabIndex = 5;
+            this.BtnPlayAgain.Text = "Play Again?";
+            this.BtnPlayAgain.UseVisualStyleBackColor = true;
+            this.BtnPlayAgain.Click += new System.EventHandler(this.BtnPlayAgain_Click);
+            // 
+            // BtnBack
+            // 
+            this.BtnBack.Location = new System.Drawing.Point(8, 380);
+            this.BtnBack.Name = "BtnBack";
+            this.BtnBack.Size = new System.Drawing.Size(94, 29);
+            this.BtnBack.TabIndex = 4;
+            this.BtnBack.Text = "<< Back";
+            this.BtnBack.UseVisualStyleBackColor = true;
+            this.BtnBack.Click += new System.EventHandler(this.BtnBack_Click);
+            // 
+            // LBLLetters
+            // 
+            this.LBLLetters.AutoSize = true;
+            this.LBLLetters.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LBLLetters.Location = new System.Drawing.Point(267, 307);
+            this.LBLLetters.Name = "LBLLetters";
+            this.LBLLetters.Size = new System.Drawing.Size(172, 46);
+            this.LBLLetters.TabIndex = 3;
+            this.LBLLetters.Text = "Blank Atm";
+            // 
+            // LBLUsed
+            // 
+            this.LBLUsed.AutoSize = true;
+            this.LBLUsed.Location = new System.Drawing.Point(53, 108);
+            this.LBLUsed.Name = "LBLUsed";
+            this.LBLUsed.Size = new System.Drawing.Size(0, 20);
+            this.LBLUsed.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.TBCMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Cam Hang Man";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.ImgHangMan)).EndInit();
+            this.TBCMain.ResumeLayout(false);
+            this.TPMenu.ResumeLayout(false);
+            this.TPDifficulty.ResumeLayout(false);
+            this.TPDifficulty.PerformLayout();
+            this.TPGame.ResumeLayout(false);
+            this.TPGame.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox ImgHangMan;
+        private Label LBLTrashHeading;
+        private TabControl TBCMain;
+        private TabPage TPMenu;
+        private TabPage TPGame;
+        private Label LBLUsed;
+        private Label LBLLetters;
+        private Button BTNPlay;
+        private Button BtnBack;
+        private Button BtnPlayAgain;
+        private TabPage TPDifficulty;
+        private Label LBLDifficulty;
+        private Button BTNEasy;
+        private Button BTNHard;
+        private Button BTNMedium;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,7 +1,191 @@
+//This file is made specifically for methods that the components themself use
 namespace Hangman {
     public partial class MainForm : Form {
+        //Random Object
+        private readonly Random rand;
+        //List of images
+        private readonly List<string> images;
+        //Global index of the current image index
+        private int imgIndex;
+        //The game state
+        private bool inGame;
+        //Difficulty being played
+        private string difficulty;
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainForm() {
-            InitializeComponent();
+            this.InitializeComponent();
+            //Position Form in the center of the screen
+            this.StartPosition = FormStartPosition.CenterScreen;
+            //Create the random object
+            this.rand = new Random();
+            //Make a new List of images
+            this.images = new();
+            //Set the starting index at 0 like it should be
+            this.imgIndex = 0;
+            //Loading all file paths into the list
+            for (int i = 0; i < 10; i++) { this.images.Add($@"{Environment.CurrentDirectory}\Assets\Hangman_{i}.png"); }
+            //Make the cursor over the image to a 'NO' cursor because your're not supposed to click on the image
+            this.ImgHangMan.Cursor = Cursors.No;
+            //Set the default image to the 1st location
+            this.ImgHangMan.ImageLocation = this.images[this.imgIndex];
+            //Hide the tab pages... idk how it works it just does
+            //link:https://stackoverflow.com/questions/10316567/how-do-i-create-a-tab-control-with-no-tab-header-in-windows-form
+            this.TBCMain.Appearance = TabAppearance.FlatButtons;
+            this.TBCMain.ItemSize = new Size(0, 1);
+            this.TBCMain.SizeMode = TabSizeMode.Fixed;
+            //Set the sizes and locations of the components
+            this.SetSizes();
+            //The game starts not being played
+            this.inGame = false;
+            //Defaulting all the strings
+            this.playWord = string.Empty;
+            this.insertedWord = string.Empty;
+            this.difficulty = string.Empty;
         }
+        /// <summary>
+        /// The method for when the form resizes
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void MainForm_Resize(object sender, EventArgs e) {
+            this.SetSizes();
+        }
+        /// <summary>
+        /// When the user clicks the image the index is increased. You were told not to by the cursor
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void ImgHangMan_Click(object sender, EventArgs e) {
+            this.ImgHangMan.ImageLocation = this.images[++this.imgIndex];
+            if (this.imgIndex == 9) {
+                this.LoseMessage("You really killed him huh. How sad....");
+                this.BtnBack.Visible = true;
+            }
+        }
+        /// <summary>
+        /// This method handles the keyboard input for the application. whenever a key is pressed
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void TBCMain_KeyPress(object sender, KeyPressEventArgs e) {
+            //Only handle the click if the game is active
+            if (this.inGame) {
+                //Start not knowing there was a change to the word
+                bool changed = false;
+                //Get the character and make it upper case
+                char letter = e.KeyChar.ToString().ToUpper()[0];
+                //Make sure the letter is a letter
+                if (!(letter >= 'A' && letter <= 'Z')) { return; }
+                //Check to see if the trash letters has contained that letter. if it has we just return
+                //We do not punish multiple inputs of the same letter
+                if (this.incorrect.Contains(letter)) { changed = true; }
+                //Default the temporary variable
+                string temp = string.Empty;
+                //Check each letter in case of double letter words eg::FOOT
+                for (int i = 0; i < this.playWord.Length; i++) {
+                    if (letter == this.playWord[i]) { //Check to see if that letter is in the word in play
+                        //Adding the letter
+                        temp += letter; 
+                        //Note there was a change
+                        changed = true;
+                    } else if (this.insertedWord[i] != '_') { //Check to see if that letter has previously been chosen
+                        temp += this.insertedWord[i];
+                    } else { //Otherwise we add a '_' to show the letter is not discovered
+                        temp += "_";
+                    }
+                }
+                //Check to see if there was no change
+                if (!changed) {
+                    //Punish the incorrect input
+                    this.ImgHangMan.ImageLocation = this.images[++this.imgIndex];
+                    //Add the letter to the trash
+                    this.TrashChars(letter);
+                    //Check to see if the game has ended
+                    if (this.imgIndex == 9) {
+                        //End the game state
+                        this.inGame = false;
+                        //Output the correct word
+                        this.LBLLetters.Text = $"The correct word '{this.playWord}'";
+                        //Display the lose message
+                        this.LoseMessage("You have lost the game!!");
+                        //Make the neccessary components visable
+                        this.BtnBack.Visible = true;
+                        this.BtnPlayAgain.Visible = true;
+                        //Exit this method here to stop the rest of the code from executing
+                        return;
+                    }
+                }
+                //Re-assign the inserted word
+                this.insertedWord = temp;
+                //Print the inserted word
+                this.Print(); 
+                //Check to see if there is a win
+                if (this.insertedWord.Equals(this.playWord)) {
+                    //End the game state
+                    this.inGame = false;
+                    //display the win message
+                    this.WinMessage("You have won the game!");
+                    //Make the necessary componants visable
+                    this.BtnBack.Visible = true;
+                    this.BtnPlayAgain.Visible = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Change the menu to the select difficulty menu
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void BTNPlay_Click(object sender, EventArgs e) {
+            this.TBCMain.SelectedTab = this.TPDifficulty;
+        }
+        /// <summary>
+        /// Play the game in 'EASY' mode
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void BTNEasy_Click(object sender, EventArgs e) {
+            this.difficulty = "EASY";
+            this.Play();
+        }
+        /// <summary>
+        /// Play the game in medium mode
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void BTNMedium_Click(object sender, EventArgs e) {
+            this.difficulty = "MEDIUM";
+            this.Play();
+        }
+        /// <summary>
+        /// Play the game in hard mode
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void BTNHard_Click(object sender, EventArgs e) {
+            this.difficulty = "HARD";
+            this.Play();
+        }
+        /// <summary>
+        /// Go back at the end of the game
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void BtnBack_Click(object sender, EventArgs e) {
+            this.TBCMain.SelectedTab = this.TPMenu;
+            this.OnFinish();
+        }
+        /// <summary>
+        /// Allow the user to play again at the same difficulty
+        /// </summary>
+        /// <param name="sender">object that raised the event</param>
+        /// <param name="e">data related to the event</param>
+        private void BtnPlayAgain_Click(object sender, EventArgs e) {
+            this.OnFinish();
+            this.Play();
+        }
+
     }
 }
